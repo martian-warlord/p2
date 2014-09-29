38863e9be88cb255a6eaddfbbf41b9570c901158
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-29 11:30:42 -0500

    initial files added to folder.  some initial word catagories added to logic

---
+++ README.md
@@ -0,0 +1,19 @@
+# p2
+
+## Live URL
+<http://p2.joshuainmadison.me>
+
+## Description
+P1: System Setup / Portfolio
+
+
+## Demo
+Screencast TDB
+
+## Details for teaching team
+T
+
+## Outside code
+
+
+
---
+++ logic.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+// MAGIC WORDS - Alagazam, HocusPocus, Abracadabra, Shazam
+
+// MAGIC SUFFIX - Obey, OpenSeseme
+
+//SUPER MAGIC WORD - AlagazamAlagazamAlagazam
+
+
+
+//  AFFIRMATION - Lucky, Happy, Loved, Awesome, 
+
+// VERBS - Love, Hate, Crave, See
+
+//FOOD - Sushi, Spam, Burgers, Apples, Cheese, DirtyFeet, 
+
+// COLOR - Red, Blue, Green, Pink, Gold, Purple, Orange, Baby
+
+//ANIMAL - Dog, Cat, Fish, Bird, Panda, Duck, Bunny, Monkey
