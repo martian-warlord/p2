7ff30af090314cb07cdd238f173beb5651351d8d
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-29 11:58:07 -0500

    Added form elements to inde page and word listage to the logic page

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title></title>
+<title>p2</title>
 
 </head>
 <body>
+
+	<form method='POST' action='logic.php'>
+    Choose a password type<br>
+   <input type="radio" name="passwordStyle" value="Affirmation">Affirmative<br>
+   <input type="radio" name="passwordStyle" value="Magickal">Magic<br>
+   <input type="radio" name="passwordStyle" value="Magickal">Secret Agent<br>
+   <input type="radio" name="passwordStyle" value="Magickal">Cognitively Enhanced
+    <input type='submit' value='Generate Your Password'><br>
+
+
+
+</form>
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -7,13 +7,13 @@
 
 //SUPER MAGIC WORD - AlagazamAlagazamAlagazam
 
+// INTERJECTIONS - Yummy, Gimme, 
 
+//  AFFIRMATION - Lucky, Happy, Loved, Awesome, strength, light, trust, wonderful, compassion, hope, courage, pleasure, light, blessings, beautiful, joy, giggles, grins, happiness, dreams, freedom, perfection, warmth, gratitude, kindness, opportunity, smiles, unity, respect, peace, wellness, success, relaxed, confidence, growing, learning, valuable, healthy, attractive, energy
 
-//  AFFIRMATION - Lucky, Happy, Loved, Awesome, 
+// VERBS - Love, Hate, Crave, See, Want, 
 
-// VERBS - Love, Hate, Crave, See
-
-//FOOD - Sushi, Spam, Burgers, Apples, Cheese, DirtyFeet, 
+//FOOD - Beans, Tacos, Rice, Fries, Sushi, Spam, Burgers, Apples, Cheese, DirtyFeet, Feta, Cheddar, Butter, Gouda, Biscuit, Steak, Ham, Bacon, Cookies, Brownies, Liver, Turkey, Beef, Noodles, Bagels, Bread, Beer, Vodka, Rum, Oats, Lard, 
 
 // COLOR - Red, Blue, Green, Pink, Gold, Purple, Orange, Baby
 
