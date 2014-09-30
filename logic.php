25767e8b6924406575a397c6d1c3257848ac742f
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-29 19:48:03 -0500

    added magic. its too much.  time to dial back

--- logic.php
+++ logic.php
@@ -3,50 +3,60 @@
 
 $foo = "";
 $happybucket = "";
-
-$magicWords = [
-    "magic1" => "Alagazam",
-    "magic2" => "HocusPocus",
-    "magic3" => "Shazam",
-    "magic4" => "SimSalaBim",
-    "magic5" => "Abracadabra",
-    "magic6" => "AlagazamAlagazamAlagazam"
-];
-
-$open = [
- "open1" => "ICommandTheeOpen",
- "open2" => "OpenSesame"
-];
+$coinflip = 1;
+$who = "";
+$hoodoo = "";
+$juju = "";
 
 
-if ($_POST["passwordStyle"] == "Affirmation"){ $foo = "Your secret password is: " . "no!";} 
-else if ($_POST["passwordStyle"] == "Magic"){ $foo = "Your secret password is: " . "goos!";} 
+
+
+if ($_POST["passwordStyle"] == "Affirmation"){ 
+$affirmations = array("Happy", "Beautiful", "Wonderful", "Lucky", "Fantastic", 
+	"Loved", "Deliscious", "Awesome", "Strong", "Sexy", "Fierce", "Powerful", "Free", "Joyful", 
+	"Peaceful", "Successful", "Relaxed", "Confident", "Grounded", "Winning", "SuperDuper", "Valuable", 
+	"Growing", "Healthy", "Attractive", "Energized", "Healthy");
+        for ($i=0; $i<=1; $i++)
+        {
+            $pickacard = array_rand($affirmations);                       
+            $happybucket .= $affirmations[$pickacard];                        
+        }
+        $foo = "Your secret password is: " . "Iam". $happybucket .  "and" ; 
+
+            $pickacard = array_rand($affirmations);                       
+            $foo .= $affirmations[$pickacard]; 
+
+} 
+else if ($_POST["passwordStyle"] == "Magic"){ 
+
+$authority = array("ByJupiter", "AlmightyIsis", "OMuses");
+$magicWords = array("Alagazam", "HocusPocus","Shazam","SimSalaBim","Abracadabra","AlagazamAlagazamAlagazam");
+$open = array("ICommandTheeOpen","OpenSesame");
+
+$foo = "Your secret password is: ";
+
+            $who = array_rand($authority);                       
+            $foo .= $authority[$who]; 
+$foo .= "HearMyCall";
+
+            $hoodoo = array_rand($magicWords);                       
+            $foo .= $magicWords[$hoodoo]; 
+
+            $juju = array_rand($open);                       
+            $foo .= $open[$juju]; 
+
+$foo .= "Obey!";
+
+} 
 else if ($_POST["passwordStyle"] == "Secret"){ $foo = "Your secret password is: " . "doo!";} 
 else if ($_POST["passwordStyle"] == "Random"){ $foo = "Your secret password is: " . "oops!";} 
 
 
-$magicWords = [
-    "magic1" => "Alagazam",
-    "magic2" => "HocusPocus",
-    "magic3" => "Shazam",
-    "magic4" => "SimSalaBim",
-    "magic5" => "Abracadabra",
-    "magic6" => "AlagazamAlagazamAlagazam"
-];
-$rand_keys = array_rand($magicWords, 5);
-echo $magicWords[$rand_keys[4]];
 
 
 
-$affirmations = array("Happy", "Beautiful", "Wonderful", "Lucky", "Loved", "Awesome", "Strong", "Free", "Joyful", "Peaceful", "Successful", "Relaxed", "Confident", "Grounded", "Valuable", "Growing", "Healthy", "Attractive", "Energized", "Healthy");
-        for ($i=0; $i<=3; $i++)
-        {
-            $pickacard = array_rand($affirmations);
-                        //this generates the random number from the array
-            $happybucket .= $affirmations[$pickacard];
-                        //here we will display the exact charachter from the array
-        }
-        echo "Iam". $happybucket; // printing result
+
+
 
 
 // INTERJECTIONS - Yummy, Gimme, 
